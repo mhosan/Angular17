7abebfa836477968eb3a522e380b6866e3b4b33c
mhosan <mhosan@gmail.com>
2024-04-17 17:21:11 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductsService } from './api/products.service';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    AsyncPipe,
+    JsonPipe
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'prueba01';
+  private readonly productsSvc = inject(ProductsService);
+  products$ = this.productsSvc.getAllProducts();
 }
